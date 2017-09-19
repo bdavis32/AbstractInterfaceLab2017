3334f88a094a0103e62efc9cc50070518ef9a668
Benjamin <Benjamin@192.168.1.128>
2017-09-19 13:49:23 -0500

    Modified Startup method

--- src/lab1/Startup.java
+++ src/lab1/Startup.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab1;
 
 /**
@@ -11,6 +6,8 @@ package lab1;
  */
 public class Startup {
     public static void main(String[] args) {
-        
+        ProgrammingCourse course1 = new AdvancedJavaCourse("Advanced Java Programming", "152-135");
+        System.out.println("Class name: " + course1.getCourseName() +
+                         "\nClass number: " + course1.getCourseNumber());
     }
 }
